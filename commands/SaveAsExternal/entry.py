fcbb20014cad025f76fef85b8243ef34ca2c5c8a
Dominic Bastien <dominic.bastien@icentia.com>
2025-03-18 11:45:51 -0400

    add Component selection

--- commands/SaveAsExternal/entry.py
+++ commands/SaveAsExternal/entry.py
@@ -29,6 +29,7 @@ ICON_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resource
 # Local list of event handlers used to maintain a reference so
 # they are not released and garbage collected.
 local_handlers = []
+handlers = []
 
 
 # Executed when add-in is run.
@@ -84,6 +85,7 @@ def command_created(args: adsk.core.CommandCreatedEventArgs):
 
     # Connect to the events that are needed by this command.
     futil.add_handler(args.command.execute, command_execute, local_handlers=local_handlers)
+    futil.add_handler(args.command.inputChanged, command_input_changed, local_handlers=local_handlers)
     futil.add_handler(args.command.destroy, command_destroy, local_handlers=local_handlers)
 
     # Get all components in the active design.
@@ -95,6 +97,17 @@ def command_created(args: adsk.core.CommandCreatedEventArgs):
     if not design:
         ui.messageBox('No active Fusion design', title)
         return
+    
+    inputs = args.command.commandInputs
+
+    # Create the title of the UI box
+    title_box = inputs.addTextBoxCommandInput('title_box', '', 'PCB Selection', 1, True)
+
+    # Create a selection input, apply filters and set the selection limits
+    selection_input = inputs.addSelectionInput('selection_input', 'Occurrence Selection', 'Select Occurence')
+    selection_input.addSelectionFilter('Occurrences')
+    selection_input.setSelectionLimits(0, 0)
+
 
 # This event handler is called when the user clicks the OK button in the command dialog or 
 # is immediately called after the created event not command inputs were created for the dialog.
@@ -102,17 +115,30 @@ def command_execute(args: adsk.core.CommandEventArgs):
     # General logging for debug.
     futil.log(f'{CMD_NAME} Command Execute Event')
 
-    # Liste of all de components
-    components = design.allComponents
-    # Find the root component to exclude it
+    inputs = args.command.commandInputs
+    selection_input : adsk.core.SelectionCommandInput = inputs.itemById('selection_input')
+    selection = selection_input.selection(0)
+    selected_entity = selection.entity
+    # # Liste of all de components
+    # components = design.allComponents
+    # # Find the root component to exclude it
     root = design.rootComponent
-    # Make the list of all the component to save
+    # # Make the list of all the component to save
     componentsToSave = []
-    for component in components:
-        # Skip the root component.
-        if root == component:
-            continue
-        componentsToSave.append(component)
+    # for component in components:
+    #     # Skip the root component.
+    #     if root == component:
+    #         continue
+    #     componentsToSave.append(component)
+
+    for j in range(0, selection_input.selectionCount):
+        try: 
+            selection = selection_input.selection(j)
+            Occurences = selection.entity
+            componentsToSave.append(Occurences.component)
+        except:
+            futil.log('Fail to find component')
+
 
     # Save as all the component
     SavedComponents = []
@@ -120,60 +146,53 @@ def command_execute(args: adsk.core.CommandEventArgs):
     New_folder_name = Root_Name  + ' external part '
     Active_Folder = app.data.activeFolder
     Save_Folder = Active_Folder.dataFolders.add(New_folder_name)
-    global k
-    k = 0
-    for component in componentsToSave:
-        # Get the name
-        name = component.name
-        doc = component.saveCopyAs(name,Save_Folder ,'','' )
-        SavedComponents.append(name)
-        # Spin until the save is complete, up to 5 seconds and then abort.
-        startTime = time.time()
-        endTime = startTime + 10
-        while not doc.uploadState and time.time() < endTime: 
-         app.log('Waiting for save to finish.')
-         state = doc.uploadState
-         app.log(str(state))
-         time.sleep(0.5)
-         adsk.doEvents()
-        app.log('File is saved')
-        state = doc.uploadState
-        app.log(str(state))
-        adsk.doEvents()
+
+    SavedComponents = save_components_event_driven(componentsToSave, Save_Folder)
     
-    componentsToDelete = componentsToSave
-    for component in componentsToDelete:
-     if component.isValid == True:
-        occurrences = root.allOccurrencesByComponent(component)
-        name = component.name
-        uniqueOccurrences = []
-        for occurrence in occurrences:
+
+    
+    # componentsToDelete = componentsToSave
+    # for component in componentsToDelete:
+    #  if component.isValid == True:
+    #     occurrences = root.allOccurrencesByComponent(component)
+    #     name = component.name
+    #     uniqueOccurrences = []
+    #     for occurrence in occurrences:
                 
-            for k in range(0, len(uniqueOccurrences)):
-                if occurrence is uniqueOccurrences[k]:
-                    break
-            if k == len(uniqueOccurrences):
-                uniqueOccurrences.append(occurrence)
+    #         for k in range(0, len(uniqueOccurrences)):
+    #             if occurrence is uniqueOccurrences[k]:
+    #                 break
+    #         if k == len(uniqueOccurrences):
+    #             uniqueOccurrences.append(occurrence)
 
-            # Delete them.
-            for uniqueOccurrencesI in uniqueOccurrences:
-                uniqueOccurrencesI.deleteMe()
+    #         # Delete them.
+    #         for uniqueOccurrencesI in uniqueOccurrences:
+    #             uniqueOccurrencesI.deleteMe()
 
-    #import file into curent design 
-    for file in Save_Folder.dataFiles:
-           root.occurrences.addByInsert(file, adsk.core.Matrix3D.create(), True) 
+    # #import file into curent design 
+    # for file in Save_Folder.dataFiles:
+    #        root.occurrences.addByInsert(file, adsk.core.Matrix3D.create(), True) 
 
 
     # Give feed back to user
-    if len(SavedComponents) == 0:
+    if SavedComponents == 0:
         msg = 'No component to save.'
     else:
-        if len(SavedComponents) > 1:
-            msg = str(len(SavedComponents)) + ' save component' + 's'
+        if SavedComponents > 1:
+            msg = str(SavedComponents) + ' save component' + 's'
         else:
-            msg = str(len(SavedComponents)) + ' save component' + ' save'
+            msg = str(SavedComponents) + ' save component' + ' save'
         msg += '\n\n'
     ui.messageBox(msg, title)
+
+# This event handler is called when the user changes anything in the command dialog
+# allowing you to modify values of other inputs based on that change.
+def command_input_changed(args: adsk.core.InputChangedEventArgs):
+    changed_input = args.input
+    inputs = args.inputs
+
+    # General logging for debug.
+    futil.log(f'{CMD_NAME} Input Changed Event fired from a change to {changed_input.id}')
 
 # This event handler is called when the command terminates.
 def command_destroy(args: adsk.core.CommandEventArgs):
@@ -182,3 +201,18 @@ def command_destroy(args: adsk.core.CommandEventArgs):
 
     global local_handlers
     local_handlers = []
+
+def save_components_event_driven(components_to_save, save_folder):
+    try:
+        component_saved = 0
+        for component in components_to_save:
+                name = component.name
+                component.saveCopyAs(name, save_folder, '', '')
+                component_saved +=1
+        return component_saved
+
+
+    except Exception as e:
+        ui.messageBox(f'Failed: {str(e)}')
+
+
